797bd2666789dadaaec471badf62c0cda459f0b6
Petri Wilhelmsen <petriw@gmail.com>
2020-03-14 08:33:09 +0100

    Apollo changes

--- ApolloMissionEditor/MissionDescription.cs
+++ ApolloMissionEditor/MissionDescription.cs
@@ -1,7 +1,6 @@
-﻿using System;
+﻿using ApolloCockpitTools;
+using System;
 using System.Collections.Generic;
-using Apollo;
-using ApolloCockpitTools;
 
 namespace Apollo.Mission
 {
@@ -26,11 +25,10 @@ namespace Apollo.Mission
         public float Inclination { get; set; }
         public string CommandModulePilot { get; set; }
         public string LunarModulePilot { get; set; }
-        public bool DisableDefaultAudioSFX { get; set; } = false;
-        public string LoadState { get; set; } = "";
+        public bool DisableDefaultAudioSFX { get; set; }
+        public string LoadState { get; set; }
         public float EarthRotationOffsetDegrees { get; set; }
-
-        public PayloadSelection Payload { get; set; } = PayloadSelection.LunarModule;
+        public PayloadSelection Payload { get; set; }
 
         public List<MissionCommand> MissionCommands = new List<MissionCommand>();
         public List<MissionGoals> MissionGoals = new List<MissionGoals>();
@@ -101,86 +99,105 @@ namespace Apollo.Mission
 
         public enum ExecuteAction
         {
-            Message = 0, // Deliver message only
+            i0000_Message = 0, // Deliver message only
 
             // 10 group. orbit checks
-            IsAp_Value1 = 10,
-            IsPe_Value1,
-            IsAp_Value1_IsPe_Value2,
-            IsInclination_Value1,
-            WaitUntilCompletedNrOrbits,
-            Orbit_TimeToPe_Value1,
-            Orbit_TimeToAp_Value1,
-            Orbit_TimeToRetro_Value1Seconds,
-            TimeToSPSBurn_Value1Seconds,
-            CheckIfApIs_Value1_YesString1_NoString2,
-            GravityInfluencer_Earth,
-            GravityInfluencer_Moon,
+            i0010_IsAp_Value1 = 10,
+            i0011_IsPe_Value1,
+            i0012_IsAp_Value1_IsPe_Value2,
+            i0013_IsInclination_Value1,
+            i0014_WaitUntilCompletedNrOrbits,
+            i0015_Orbit_TimeToPe_Value1,
+            i0016_Orbit_TimeToAp_Value1,
+            i0017_Orbit_TimeToRetro_Value1Seconds,
+            i0018_TimeToSPSBurn_Value1Seconds,
+            i0019_CheckIfApIs_Value1_YesString1_NoString2,
+            i0020_GravityInfluencer_Earth,
+            i0021_GravityInfluencer_Moon,
 
             // 50 group - radio checks
-            IsAtRadioStation_String1 = 50,
+            i0050_IsAtRadioStation_String1 = 50,
 
             // 100 group - altitude and such
-            AltitudeLess_Value1 = 100,
-            AltitudeGreater_Value1,
-            AtCoordinate_Value1_Value2,
-            Altitude_Landing_Feet_LessThan_Value1,
-            Longitude_Between_Value1_Value2,
-            Latitude_Between_Value1_Value2,
+            i0100_AltitudeLess_Value1 = 100,
+            i0101_AltitudeGreater_Value1,
+            i0102_AtCoordinate_Value1_Value2,
+            i0103_Altitude_Landing_Feet_LessThan_Value1,
+            i0104_Longitude_Between_Value1_Value2,
+            i0105_Latitude_Between_Value1_Value2,
 
             // 150 group - spacecraft specific
-            Seperation_StageIC = 150,
-            Seperation_StageII,
-            Seperation_StageIVB,
-            Seperation_StageSM,
-            State_Dot05GDetected,
-            State_InHiDragZone,
-            State_OutsideHighDragZone,
-            State_DrogueDeploy,
-            State_MainDeploy,
-            State_UmbillicalDisconnect,
-            State_Splashdown,
-            State_TowerJett,
-            State_AscentComplete,
-            State_CenterEngineCutoff,
-            State_SPSBurning,
-            State_SPSNotBurning,
-            State_AnyEngineBurning,
-            State_NoEnginesBurning,
+            i0150_Seperation_StageIC = 150,
+            i0151_Seperation_StageII,
+            i0152_Seperation_StageIVB,
+            i0153_Seperation_StageSM,
+            i0154_State_Dot05GDetected,
+            i0155_State_InHiDragZone,
+            i0156_State_OutsideHighDragZone,
+            i0157_State_DrogueDeploy,
+            i0158_State_MainDeploy,
+            i0159_State_UmbillicalDisconnect,
+            i0160_State_Splashdown,
+            i0161_State_TowerJett,
+            i0162_State_AscentComplete,
+            i0163_State_CenterEngineCutoff,
+            i0164_State_SPSBurning,
+            i0165_State_SPSNotBurning,
+            i0166_State_AnyEngineBurning,
+            i0167_State_NoEnginesBurning,
 
             // 200 group
-            CheckPin_PinID_PinPosition = 200,
-            CheckKnobIdSelection_Value1,
-            CheckTriggerButton,
-            CheckFuse_FusePosition,
-            CheckIfAGC_Program_Value1_IsRunning,
+            i0200_CheckPin_PinID_PinPosition = 200,
+            i0201_CheckKnobIdSelection_Value1,
+            i0202_CheckTriggerButton,
+            i0203_CheckFuse_FusePosition,
+            i0204_CheckIfAGC_Program_Value1_IsRunning,
 
             // 300 group - LM
-            LunarModule_CheckPin_PinID_PinPosition = 300,
-            LunarModule_CheckFuse_FusePosition,
-            LunarModule_CheckKnobIdSelection_Value1,
-            LunarModule_CheckTriggerButton,
-            LunarModule_CheckIfAGC_Program_Value1_IsRunning,
-            LunarModule_SwitchedToLunarStayArea,
-            LunarModule_LunarContact,
+            i0300_LunarModule_CheckPin_PinID_PinPosition = 300,
+            i0301_LunarModule_CheckFuse_FusePosition,
+            i0302_LunarModule_CheckKnobIdSelection_Value1,
+            i0303_LunarModule_CheckTriggerButton,
+            i0304_LunarModule_CheckIfAGC_Program_Value1_IsRunning,
+            i0305_LunarModule_SwitchedToLunarStayArea,
+            i0306_LunarModule_LunarContact,
+            i0307_LunarModule_DPSBurning_Bool1,
+            i0308_LunarModule_APSBurning_Bool1,
+            i0309_LunarModule_Velocity_Value1_GreaterOrLess_Bool1,
+            i0310_LunarModule_DistanceToCSM_LessOrEqual_Value1_Feet,
 
             // 400 group - stats
-            Velocity_GreaterThan_Value1_FPS = 400,
-            Velocity_LessThan_Value1_FPS,
+            i0400_Velocity_GreaterThan_Value1_FPS = 400,
+            i0401_Velocity_LessThan_Value1_FPS,
 
-            // 500 group
-            Set_PinID_to_PinPosition = 500,
-            Set_LM_PinID_to_LM_PinPosition,
+            // 500 group - auto set
+            i0500_Set_PinID_to_PinPosition = 500,
+            i0501_LunarModule_Set_LM_PinID_to_LM_PinPosition,
 
             // 750 group - PADs
-            RequestTLI_PAD_LambdaValue1 = 750,
+            i0750_RequestTLI_PAD_LambdaValue1 = 750,
 
             // 1000 group
-            ResetTimestamp = 1000,
-            ActivityCompleted_Value1,
-            StartFailiure,
-            StopFailiure,
-            CompleteMission
+            i1000_ResetTimestamp = 1000,
+            i1001_ActivityCompleted_Value1,
+            i1002_StartFailiure,
+            i1003_StopFailiure,
+            i1004_CompleteMission,
+
+            // 2000 group - CSM State
+            // 3000 group - LM State
+            i3012_LunarModule_Sequencer_IsComplete_P12Guidance = 3012,
+            i3063_LunarModule_Sequencer_IsComplete_P63Guidance = 3063,
+            i3064_LunarModule_Sequencer_IsComplete_P64Guidance = 3064,
+            i3100_LunarModule_Sequencer_IsComplete_LunarDescent = 3100,
+            i3101_LunarModule_Sequencer_IsComplete_LunarAscent,
+            i3102_LunarModule_Sequencer_IsComplete_CSI,
+            i3103_LunarModule_Sequencer_IsComplete_PC,
+            i3104_LunarModule_Sequencer_IsComplete_CDH,
+            i3105_LunarModule_Sequencer_IsComplete_TPI,
+            i3106_LunarModule_Sequencer_IsComplete_TPM,
+            i3107_LunarModule_Sequencer_IsComplete_TPF,
+            i3108_LunarModule_Sequencer_IsComplete_DockedWithCSMAfterAscent
         }
 
         public ExecuteAction Action { get; set; }
@@ -202,7 +219,7 @@ namespace Apollo.Mission
         public TriggerButtonID ButtonId { get; set; }
         public FuseID FuseID { get; set; }
         public FusePosition FusePosition { get; set; }
-        
+
         public LunarModule.CockpitTools.PinID LM_PinID { get; set; }
         public LunarModule.CockpitTools.PinPosition LM_PinPosition { get; set; }
         public LunarModule.CockpitTools.KnobID LM_KnobID { get; set; }
